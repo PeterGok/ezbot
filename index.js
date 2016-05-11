d8baf6d1c4bb25a3e31aa56d2a221691333ee80a
Peter Gokhshteyn <petergok@v1020-wn-123-113.campus-dynamic.uwaterloo.ca>
2016-05-11 18:26:50 -0400

    Fixed bug in index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.set('port', Number(process.env.PORT || 8080));
+
 app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === "blah_kevin_blah") {
+  if (req.query['hub.verify_token'] === 'blah_kevin_blah') {
     res.send(req.query['hub.challenge']);
   } else {
     res.send('Error, wrong validation token');    
   }
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
 });
