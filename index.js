f08a451b7d368af3403e178b834c4da60826d582
Peter Gokhshteyn <petergok@v1020-wn-123-113.campus-dynamic.uwaterloo.ca>
2016-05-11 21:20:10 -0400

    Added a simple script to get user's address

--- index.js
+++ index.js
@@ -10,6 +10,65 @@ var token = "EAADqBHxn6U4BAN8GksPJ0fQS5GaVZA03Oc9x0Nj4OolLnGFmvfmetYA7dgXqt7jUJd
 
 console.log("hello");
 
+var ConversationState = {
+  BEFORE: 0,
+  PROMPT: 1,
+  RESPONDED_YES: 2,
+  RESPONDED_NO: 3
+};
+
+var AddressType = {
+  INVALID: 0,
+  NOT_SERVICEABLE: 1,
+  SERVICEABLE: 2
+}
+
+var conversations = {};
+
+function Conversation (type) {
+  this.state = ConversationState.BEFORE;
+  this.user_address = "";
+  this.valid_address = false;
+}
+
+function verifyAddress(address) {
+  return AddressType.SERVICEABLE;
+}
+
+Conversation.prototype.onMessage(event) {
+  var sender = event.sender.id;
+
+  if (!event.message || !event.message.text) {
+    sendTextMessage(sender, "Sorry, invalid message. Please try again.");
+  }
+
+  if (this.state == ConversationState.BEFORE) {
+    text = "Hello! Intersted in a free quote for your property? Please respond back with your address.";
+    this.state = ConversationState.PROMPT;
+  } else if (this.state == ConversationState.PROMPT) {
+    var address = event.message.text;
+    var addressType = verifyAddress(address);
+
+    if (addressType == AddressType.INVALID) {
+      sendTextMessage(sender, "Sorry, that's not a valid address, please try again.");
+    } else if (addressType == AddressType.NOT_SERVICEABLE) {
+      sendTextMessage(sender, "Sorry, we don't currently service that area, but we are hoping to roll out soon!");
+      this.state = ConversationState.RESPONDED_NO;
+      conversations[sender] = null;
+    } else {
+      sendTextMessage(sender, "Thanks. Depending on whether you want weekly or bi weekly service we can service your property for $15/week or $23 every two weeks");
+      sendTextMessage(sender, "This will include, hedging, lawn mowing, pruning, and blowing.");
+      sendTextMessage(sender, "Additionally we offer dog waste pickup and weed spraying (with roundup) for an extra charge.");
+      sendTextMessage(sender, "Click the link below if you would like to sign up:");
+      sendTextMessage(sender, "www.ezhome.com");
+      this.state = ConversationState.RESPONDED_YES;
+      conversations[sender] = null;
+    }
+  } else {
+    sendTextMessage(sender, "Please visit our website for more information: www.ezhome.com");
+  }
+}
+
 app.set('port', Number(process.env.PORT || 8080));
 
 app.get('/', function (req, res) {
@@ -17,17 +76,21 @@ app.get('/', function (req, res) {
 });
 
 app.post('/webhook/', function (req, res) {
-
     var messaging_events = req.body.entry[0].messaging;
 
     for (var i = 0; i < messaging_events.length; i++) {
-
         var event = req.body.entry[0].messaging[i];
         var sender = event.sender.id;
 
+        var conversation = conversations[sender];
+        if (!conversation) {
+          converstions[sender] = new Conversation();
+        }
+
+        conversations[sender].onMessage(event);
+
         if (event.message && event.message.text) {
             var text = event.message.text;
-
             sendTextMessage(sender, "Echo: " + text.substring(0, 200));
         }
     }
@@ -44,7 +107,6 @@ app.get('/webhook', function (req, res) {
 });
 
 function sendTextMessage(sender, text) {
-
     var messageData = {
         text: text
     };
@@ -58,15 +120,12 @@ function sendTextMessage(sender, text) {
             message: messageData
         }
     }, function (error, response) {
-
         if (error) {
             console.log('Error sending message: ', error);
         } else if (response.body.error) {
             console.log('Error: ', response.body.error);
         }
-
     });
-
 }
 
 app.listen(app.get('port'), function(){
